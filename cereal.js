4a5f7c7e73df46b35f9f589ac975748957cf634c
neil patel <neil@myindianweddingapp.com>
2019-03-24 11:39:32 +0000

    Added font weights and transitions

--- cereal.js
+++ cereal.js
@@ -6,10 +6,11 @@ document.addEventListener("mousemove", function (event) {
         const dx = div.offsetLeft + 50 - x;
         const dy = div.offsetTop + 50 - y;
         const dist = Math.sqrt(dx * dx + dy * dy)
-        const score = Math.exp(dist * -0.003)
+        const score = Math.exp(dist * -0.004)
 
         // div.innerHTML = score.toFixed(2);
         div.style.transform = "scale(" + score + ")"
+        div.style.fontWeight = 100 + (100 * Math.round(8 * score))
     })
 })
 
