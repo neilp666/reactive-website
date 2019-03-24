aeb8e6e0ce51d149fffe7ed9080264b8b9b5e3c5
neil patel <neil@myindianweddingapp.com>
2019-03-24 11:31:35 +0000

    added scaling to the individual letters

--- cereal.js
+++ cereal.js
@@ -6,8 +6,10 @@ document.addEventListener("mousemove", function (event) {
         const dx = div.offsetLeft + 50 - x;
         const dy = div.offsetTop + 50 - y;
         const dist = Math.sqrt(dx * dx + dy * dy)
+        const score = Math.exp(dist * -0.003)
 
-        div.innerHTML = Math.round(dist);
+        // div.innerHTML = score.toFixed(2);
+        div.style.transform = "scale(" + score + ")"
     })
 })
 
